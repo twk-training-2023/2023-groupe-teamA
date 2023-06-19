f03545d4fa37213a691c5cce814cc33d0d5e9ba0
momozakim0 <136281950+momozakim0@users.noreply.github.com>
2023-06-19 14:52:28 +0900

    ログインからメインメニューまでを追加しました。

---
+++ WebProject/WEB-INF/src/model/LoginInfo.java
@@ -0,0 +1,26 @@
+package model;
+
+public class LoginInfo {
+	private int employeeID;//	社員番号
+	private String name;//	名前
+	private int permissionLevel;//	権限レベル
+	
+	public int getEmployeeID() {
+		return employeeID;
+	}
+	public void setEmployeeID(int employeeID) {
+		this.employeeID = employeeID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPermissionLevel() {
+		return permissionLevel;
+	}
+	public void setPermissionLevel(int permissionLevel) {
+		this.permissionLevel = permissionLevel;
+	}
+}
